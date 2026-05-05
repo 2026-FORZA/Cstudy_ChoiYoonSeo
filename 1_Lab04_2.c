a26026173a32b47c794acdb1fa7ef7adcaf2e988
yoonseo-ys <cys26@sookmyung.ac.kr>
2026-05-05 11:02:26 +0900

    1_Lab04

--- 1_Lab04_2.c
+++ 1_Lab04_2.c
@@ -1,6 +1,17 @@
-﻿오전 10:52에 빌드를 시작함...
-1>------ 빌드 시작: 프로젝트: Project3, 구성: Debug x64 ------
-1>  1_Lab04_02.c
-1>  Project3.vcxproj -> C:\Users\sally\OneDrive\Desktop\Project3\x64\Debug\Project3.exe
-========== 빌드: 1개 성공, 0개 실패, 0개 최신 상태, 0개 건너뜀 ==========
-========== 빌드이(가) 오전 10:52에 완료되었으며, 01.445 초이(가) 걸림 ==========
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+
+	int i, j, n;
+	scanf("%d", &n);
+
+	for (i = 1; i <= n;i++) {
+		for (j = 1; j <= n;j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
