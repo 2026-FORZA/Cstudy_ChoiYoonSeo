9a6d2e0864267e24780054d4de607682a1613456
yoonseo-ys <cys26@sookmyung.ac.kr>
2026-05-12 13:46:05 +0900

    1_Lab05

---
+++ 1_Lab05_1.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int k;
+
+void f(int k)
+{
+	if (k % 2 == 0)
+		printf("even");
+	else
+		printf("odd");
+}
+
+int main()
+{
+	scanf("%d", &k);
+	f(k);
+	return 0;
+}
