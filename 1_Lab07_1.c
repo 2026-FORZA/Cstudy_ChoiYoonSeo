8b8ccc5cacb0a24fd99067d801149bc5d2285891
yoonseo-ys <cys26@sookmyung.ac.kr>
2026-05-27 23:08:44 +0900

    1_Lab07

---
+++ 1_Lab07_1.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+
+    char str[1001];
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (isupper(str[i]))
+            str[i] = tolower(str[i]);
+
+        else if (islower(str[i]))
+            str[i] = toupper(str[i]);
+    }
+
+    printf("%s", str);
+    return 0;
+}
